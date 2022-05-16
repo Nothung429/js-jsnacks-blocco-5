727f7541c0578d578302d631bf616eee9b414632
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-16 12:47:12 +0200

    fix js

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,17 +6,17 @@ const zucchine = [
     {
         varietà: "varietà 1",
         peso: 100,
-        lunghezza: 5
+        lunghezza: 6
     },
     {
         varietà: "varietà 2",
         peso: 200,
-        lunghezza: 15
+        lunghezza: 16
     },
     {
         varietà: "varietà 3",
         peso: 300,
-        lunghezza: 5
+        lunghezza: 4
     },
     {
         varietà: "varietà 4",
@@ -26,32 +26,32 @@ const zucchine = [
     {
         varietà: "varietà 5",
         peso: 500,
-        lunghezza: 5
+        lunghezza: 8
     },
     {
         varietà: "varietà 6",
         peso: 600,
-        lunghezza: 15
+        lunghezza: 18
     },
     {
         varietà: "varietà 7",
         peso: 700,
-        lunghezza: 5
+        lunghezza: 9
     },
     {
         varietà: "varietà 8",
         peso: 800,
-        lunghezza: 15
+        lunghezza: 19
     },
     {
         varietà: "varietà 9",
         peso: 900,
-        lunghezza: 5
+        lunghezza: 3
     },
     {
         varietà: "varietà 10",
         peso: 1000,
-        lunghezza: 15
+        lunghezza: 13
     }
 ];
 
