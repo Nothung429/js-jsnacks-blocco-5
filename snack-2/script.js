c36357f0bc0340654b8eb6345062f73412e0d680
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-16 12:47:29 +0200

    fix commenti

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,6 +1,6 @@
-// Crea 10 oggetti che rappresentano una zucchina.
-// Dividi in due array separati le zucchine che misurano meno o più di 15cm.
-// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+// Crea 10 oggetti che rappresentano una zucchina
+// Dividi in due array separati le zucchine che misurano meno o più di 15cm
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine
 
 const zucchine = [
     {
