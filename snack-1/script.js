cb4b7156033fba38f62cca924e6a90fc0573df6b
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-16 12:14:52 +0200

    aggiunta file

---
+++ snack-1/script.js
@@ -0,0 +1,7 @@
+// Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza
+// Calcola quanto pesano tutte le zucchine
+const zucchine = [
+    {
+        
+    }
+]
